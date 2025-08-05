42eba4b6eeb5c392459bf3d9ea49435e9f1dedaa
mohammadfaisalkhatri@gmail.com <mohammadfaisalkhatri@gmail.com>
2025-08-05 10:37:09 +0300

    updated dependency and plugin versions in pom.xml, formatted all classes

--- src/test/java/io/github/mfaisalkhatri/pages/lambdatestecommerce/BlogPage.java
+++ src/test/java/io/github/mfaisalkhatri/pages/lambdatestecommerce/BlogPage.java
@@ -1,12 +1,13 @@
 package io.github.mfaisalkhatri.pages.lambdatestecommerce;
 
-import org.openqa.selenium.By;
-
 import static io.github.mfaisalkhatri.setup.DriverManager.getDriver;
+
+import org.openqa.selenium.By;
 
 public class BlogPage {
 
     public String pageHeader () {
-        return getDriver().findElement(By.cssSelector(".entry-section div.mz-tab-listing-header h3")).getText();
+        return getDriver ().findElement (By.cssSelector (".entry-section div.mz-tab-listing-header h3"))
+            .getText ();
     }
 }
--- src/test/java/io/github/mfaisalkhatri/tests/UITestingPlaygroundTests.java
+++ src/test/java/io/github/mfaisalkhatri/tests/UITestingPlaygroundTests.java
@@ -1,23 +1,23 @@
 package io.github.mfaisalkhatri.tests;
 
-import io.github.mfaisalkhatri.pages.uitestingplayground.OverLapElementPage;
-import org.testng.annotations.Test;
-
 import static io.github.mfaisalkhatri.setup.DriverManager.getDriver;
 import static org.testng.Assert.assertEquals;
 
-public class UITestingPlaygroundTests extends BaseTest{
+import io.github.mfaisalkhatri.pages.uitestingplayground.OverLapElementPage;
+import org.testng.annotations.Test;
+
+public class UITestingPlaygroundTests extends BaseTest {
 
     @Test
-    public void testOverLappingElement() {
-        getDriver().get("http://www.uitestingplayground.com/overlapped");
+    public void testOverLappingElement () {
+        getDriver ().get ("http://www.uitestingplayground.com/overlapped");
 
-        final var overLapElementPage = new OverLapElementPage();
+        final var overLapElementPage = new OverLapElementPage ();
         final String id = "ID7763";
         final String name = "Faisal K";
-        overLapElementPage.fillForm(id, name);
+        overLapElementPage.fillForm (id, name);
 
-        assertEquals(overLapElementPage.textInNameField(), name);
+        assertEquals (overLapElementPage.textInNameField (), name);
 
     }
 }
